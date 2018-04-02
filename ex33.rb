c859f587cf3bb9e7d79ba9ffd1a7d20045fc4fc5
martinezk <misskristinamartinez@gmail.com>
2018-04-02 11:08:46 -0400

    Ex33 Comparing loop types

--- ex33.rb
+++ ex33.rb
@@ -16,12 +16,22 @@ def printing_numbers(number, addition)
 end
 
 printing_numbers(9, 1)
-printing_numbers(19, 3)
-printing_numbers(99, 40)
+# printing_numbers(19, 3)
+# printing_numbers(99, 40)
 
-def printing_numbers_v2(number, addition)
+def printing_numbers_v2(number, increment)
   numbers = []
+  range = 0..number
+  range.step(increment) do |i|
+    puts "At the top, i is #{i}"
+    numbers.push(i)
 
+    puts "At the bottom, i is #{i}"
 
-  numbers.each do |num|
+    puts "The numbers: "
+    numbers.each {|num| puts num}
+  end
+
 end
+
+printing_numbers_v2(10, 2)
